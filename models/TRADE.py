e202f4fd4c92412c38b6cb0f16d8f41a815298fd
joshualemmon <joshua.lemmon@uoit.net>
2020-03-18 13:01:12 -0400

    check output tuple length

--- models/TRADE.py
+++ models/TRADE.py
@@ -317,6 +317,7 @@ class EncoderBERT(nn.Module):
         embedded = self.embedding(input_seqs)
         embedded = self.dropout_layer(embedded)
         output = self.BERT(inputs_embeds=embedded)
+        print(len(output))
         outputs = output[0]
         hidden = output[1]
         return outputs.transpose(0,1), hidden.unsqueeze(0)
