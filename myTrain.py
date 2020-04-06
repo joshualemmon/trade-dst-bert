e38e21c822987b7c39b9d181e0595a5d33613544
joshualemmon <joshua.lemmon@uoit.net>
2020-04-06 10:36:32 -0400

    potential loss fix for bert model

--- myTrain.py
+++ myTrain.py
@@ -48,7 +48,6 @@ for epoch in range(200):
         # exit(1)
 
     if((epoch+1) % int(args['evalp']) == 0):
-        model.save_model('-base-all-domains')
         acc = model.evaluate(dev, avg_best, SLOTS_LIST[2], early_stop)
         model.scheduler.step(acc)
 
