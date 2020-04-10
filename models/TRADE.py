712c7f23b56786e269c96d72540cbeeeac96727c
joshualemmon <joshua.lemmon@uoit.net>
2020-04-10 12:23:35 -0400

    try adding save folder to path

--- models/TRADE.py
+++ models/TRADE.py
@@ -16,7 +16,7 @@ import json
 # import pandas as pd
 import copy
 import sys
-sys.path.insert(0, './save')
+sys.path.insert(0, '/u3/jmlemmon/cs886/Project/save')
 
 from utils.measures import wer, moses_multi_bleu
 from utils.masked_cross_entropy import *
