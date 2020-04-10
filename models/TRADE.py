1bdf2d1a16fc804f6d5606c2ab7949ad52a57256
joshualemmon <joshua.lemmon@uoit.net>
2020-04-10 12:38:22 -0400

    fixed path issue

--- models/TRADE.py
+++ models/TRADE.py
@@ -49,10 +49,6 @@ class TRADE(nn.Module):
         self.decoder = Generator(self.lang, self.encoder.embedding, self.lang.n_words, hidden_size, self.dropout, self.slots, self.nb_gate) 
         
         if path:
-            f = open('save/TRADE-multiwozdst/HDD400BSZ4DR0.2-base-all-domains/file1.txt', 'r')
-            print(f.readline())
-            print('save/TRADE-multiwozdst/HDD400BSZ4DR0.2-base-all-domains/file1.txt')
-            print(str(path)+'/enc.th')
             if USE_CUDA:
                 print("MODEL {} LOADED".format(str(path)))
                 trained_encoder = torch.load(str(path)+'/enc.th')
